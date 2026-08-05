f3c74147a0de75b32c501cf4db261712b2e2f481
abrahamuchos <j.abraham29@gmail.com>
2026-08-05 15:17:55 -0400

    Add use cases

---
+++ lib/features/trip_tracking/domain/repositories/trip_tracking_repository.dart
@@ -0,0 +1,13 @@
+import 'package:bus_tracker/core/error/failures.dart';
+import 'package:bus_tracker/features/trip_tracking/domain/entities/BusLocationEntity.dart';
+import 'package:bus_tracker/features/trip_tracking/domain/entities/trip.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class TripTrackingRepository{
+
+  Future<Either<Failure, TripEntity>> getActiveTrip();
+
+  Stream<Either<Failure, BusLocationEntity>> watchBusLocation(int tripId);
+  
+  Future<void> stopWatching();
+}
---
+++ lib/features/trip_tracking/domain/usecases/watch_bus_location.dart
@@ -0,0 +1,26 @@
+import 'package:bus_tracker/core/error/failures.dart';
+import 'package:bus_tracker/features/trip_tracking/domain/entities/BusLocationEntity.dart';
+import 'package:bus_tracker/features/trip_tracking/domain/repositories/trip_tracking_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+class WatchBusLocationParams extends Equatable {
+  final int tripId;
+
+  const WatchBusLocationParams(this.tripId);
+
+  @override
+  List<Object?> get props => [tripId];
+}
+
+class WatchBusLocation {
+  final TripTrackingRepository repository;
+
+  WatchBusLocation(this.repository);
+
+  Stream<Either<Failure, BusLocationEntity>> call(
+    WatchBusLocationParams params,
+  ) {
+    return repository.watchBusLocation(params.tripId);
+  }
+}
